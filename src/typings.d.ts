50dcc7e6d7e46233caab470709868ff991db4976
KwakesProject <kwakes@me.com>
2016-12-13 11:50:42 +0000

    first set up done using firebase externally. now to set up via npm

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,5 @@
 // Typings reference file, you can add your own global typings here
+// https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+
+declare var System: any;
