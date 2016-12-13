c8e7aaa0d5777836f279a5dfd349156dda427f6a
KwakesProject <kwakes@me.com>
2016-12-13 17:31:43 +0000

    added search filter function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,22 +3,29 @@ import { LessonsService } from './../shared/model/lessons.service';
 import { Lesson } from '../shared/model/lesson';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  lessons: Lesson[];
+    allLessons: Lesson[];
+    filtered: Lesson[];
 
-  constructor(private lessonsService: LessonsService) { }
+    constructor(private lessonsService: LessonsService) { }
 
-  ngOnInit() {
-    this.lessonsService.findAllLessons()
-      .do(console.log)
-      .subscribe(
-      lessons => this.lessons = lessons
-      );
-  }
+    ngOnInit() {
+        this.lessonsService.findAllLessons()
+            .do(console.log)
+            .subscribe(
+            lessons => this.allLessons = this.filtered = lessons
+            );
+    }
+
+    search(search: string) {
+        this.filtered = this.allLessons.filter(
+            lesson => lesson.description.includes(search)
+        );
+    }
 
 }
