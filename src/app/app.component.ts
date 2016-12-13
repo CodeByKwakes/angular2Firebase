afb2942b9a8dc4a5bf98ac796d240cea877daae5
KwakesProject <kwakes@me.com>
2016-12-13 11:55:30 +0000

    installed firebase via npm and working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { initializeApp, database } from 'firebase';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+
+    // Initialize Firebase
+    var config = {
+      apiKey: 'AIzaSyBrQrhWHHYEfj5vcUmKgyJlGFmf06WdpmE',
+      authDomain: 'final-angular-firebase.firebaseapp.com',
+      databaseURL: 'https://final-angular-firebase.firebaseio.com',
+      storageBucket: 'final-angular-firebase.appspot.com',
+      messagingSenderId: '500802307737'
+    };
+    initializeApp(config);
+
+    var root = database().ref('messages');
+
+    root.on('value', function (snap) {
+      console.log(snap.key, snap.val());
+    });
+
+  }
 }
